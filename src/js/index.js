740632cad67b31359f0e5bb780dcc3961cc50e89
Antony Córdoba <anfa2091@gmail.com>
2023-05-05 20:25:13 -0500

    Work process

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import GalleryApi from "./gallery-fetch";
