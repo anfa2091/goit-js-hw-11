4349dbccb25182be0515697da992e4056af8d6aa
Antony Córdoba <anfa2091@gmail.com>
2023-05-06 17:13:43 -0500

    work finished

--- src/js/index.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import GalleryApi from "./gallery-fetch";
